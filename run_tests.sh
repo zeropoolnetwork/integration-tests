b58f4f62e7c30136dde958cc8580c0dee45f31c7
Dmitry Vdovin <voidxnull@gmail.com>
2023-03-21 20:50:09 +0400

    Fix run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,12 @@ yarn --cwd ./test-client build
 echo "Serving the client..."
 python3 -m http.server 3000 --directory ./test-client/dist &
 
+cleanup() {
+  kill %1
+}
+
+trap cleanup EXIT INT
+
 echo "Running the selenium test..."
 python3 selenium_test.py
 
-kill $!
